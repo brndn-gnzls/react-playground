be0174f6eef6c275e1fab56eef6584a49e2a1085
AE06909 <wes.gonzales@elevancehealth.com>
2024-02-26 09:24:07 -0500

    props

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+const a = "John"
+const b = "Sam"
+const c = "Tom"
+const d = "Jack"
+
+export default d
+export {a,b}
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+import xyz from "./module1.mjs"
+import {a,b} from "./module1.mjs"
+
+console.log(xyz)
+console.log(a, b)
+
+
