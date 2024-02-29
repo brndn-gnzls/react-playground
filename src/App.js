09915523e885a5f9e57819bb48e5da84627e280d
AE06909 <wes.gonzales@elevancehealth.com>
2024-02-29 15:10:16 -0500

    Basic state usage.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer";
-
+import { useState } from 'react'
 import './App.css';
 
 const App = () => {
+    let [count, setCount] = useState(0)
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const handleDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    const handleReset = () => {
+        setCount(0)
+    }
+
     return (
         <>
-            <Header />
-            <div>
-                <h3>Lorem</h3>
+            <div className="App">
+                <div className='box'>
+                    <p>{count}</p>
+                    <button className="add" onClick={handleIncrement}>ADD</button>
+                    <button className="sub" onClick={handleDecrement}>SUB</button>
+                    <button className="res" onClick={handleReset}>RESET</button>
+                </div>
             </div>
-            <Footer/>
         </>
     )
 }
