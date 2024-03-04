5efcc9ec3508c9ecd5edb1ecab3ed4c4af2deb6a
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-04 12:16:28 -0500

    JSON Server Setup.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import Header from './components/Header';
-import TaskList from './components/TaskList';
-import Footer from './components/Footer';
-import AddTask from './components/AddTask';
-import { useState } from 'react';
 import './App.css';
 
-const App = () => {
-
-    const [tasks, setTasks] = useState([
-        {
-            id: 1478,
-            name: "Lorem Ipsum",
-            completed: true
-        },
-        {
-            id: 2109,
-            name: "Dolor Nurit",
-            completed: false
-        },
-        {
-            id: 3756,
-            name: "Solfit Preceptum",
-            completed: true
-        },
-    ])
-
-    return (
-        <>
-            <div className="App">
-                <Header />
-                <main>
-                    <AddTask tasks={tasks} setTasks={setTasks} />
-                    <TaskList tasks={tasks} setTasks={setTasks} />
-                </main>
-                <Footer />
-            </div>
-        </>
-    )
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
-export default App
+export default App;
