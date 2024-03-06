d0a84792db769f7665a7728ab1edd501583ec7e1
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-06 10:40:45 -0500

    Implemented generic hook for retrieving data from an API.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,11 @@
 // useEffect only runs when mounting or when we
 // tell it to.
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState } from 'react'
+import useFetch from '../hooks/useFetch'
 
 const ProductList = () => {
-
-    const [products, setProducts] = useState([])
     const [url, setUrl] = useState("http://localhost:9090/products")
-
-    // This is a function for which we are dependent; therefore,
-    // we utilize `useCallack`. This returns a memoized function,
-    // keeping the function the same -- this is why the dependency
-    // is added.
-    const getProducts = useCallback(async () => {
-        const response = await fetch(url)
-        const data = await response.json()
-        setProducts(data)
-    }, [url]) // dependency since function return is cached.
-
-    // useEffect for side-effects, such as fetching data.
-    useEffect(() => {
-        getProducts() // Function to get data.
-        // [] -- this second argument watches for a changing item.
-        // whenever this changes, useEffect runs again.
-    }, [getProducts]) // Same function as our dependency.
+    const { data: products } = useFetch(url)
 
     return (
         <section>
@@ -30,7 +13,8 @@ const ProductList = () => {
                 <button onClick={() => setUrl("http://localhost:9090/products")}>All</button>
                 <button onClick={() => setUrl("http://localhost:9090/products?in_stock=true")}>In-Stock Only</button>
             </div>
-            {products.map((product) => (
+            {/* products is initially set to null, first run is null, map will not work on null*/}
+            {products && products.map((product) => (
                 <div className='card' key={product.id}>
                     <p className='id'>{product.id}</p>
                     <p className='name'>{product.name}</p>
