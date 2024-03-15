f7587c55e66d95ffadad3f05b97d2440cbef8863
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-15 11:10:41 -0400

    Initial route setup.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import Home  from "./components/Home"
+import ProductList from "./components/ProductList"
+import ProductDetail from "./components/ProductDetail"
+import Contact from "./components/Contact"
 import './App.css';
-import ProductList from './components/ProductList';
 
 function App() {
     return (
         <div className="App">
-            <p>Hello World</p>
+            <header>Header</header>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="products" element={<ProductList />}></Route>
+                <Route path="products/1001" element={<ProductDetail />}></Route>
+                <Route path="contact" element={<Contact/>}></Route>
+            </Routes>
+            <footer>Footer</footer>
         </div>
     );
 }
